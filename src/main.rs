d56dd22ded38a9f888f4fd0d8918bc4eba5a6920
Jeremy Audet <jerebear@protonmail.com>
2019-07-15 15:27:59 -0400

    Refactor src/main.rs
    This file is the entry point for the application, and it's mainly
    concerned with figuring out which CLI arguments the user passed, and
    handing off responsibility once that's been sorted out. Killjoy has a
    simple CLI, but it's just complicated enough to make the CLI parsing
    logic hard to understand.
    
    Refactor this file by creating a collection of `handle_*_subcommand`
    functions, where each handles the logic for just one subcommand. This
    greatly reduces the amount of nested logic.

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,56 @@
 use std::process;
 
-use killjoy::{cli, settings};
+use clap::ArgMatches;
 
+use killjoy::{cli, settings, settings::Settings};
+
+// The entry point for the application.
 fn main() {
     let args = cli::get_cli_args();
     match args.subcommand() {
-        ("settings", Some(args_settings)) => match args_settings.subcommand() {
-            ("load-path", Some(_)) => {
-                let load_path = match settings::get_load_path() {
-                    Ok(load_path) => load_path,
-                    Err(err) => {
-                        eprintln!("{}", err);
-                        process::exit(1);
-                    }
-                };
-                println!("{}", load_path);
-            }
-            ("validate", Some(_)) => {
-                println!("Handling validate subcommand");
-            }
-            _ => {
-                eprintln!("An unexpected code path executed. Please contact the developer.");
-            }
-        },
-        _ => {
-            let settings_obj = match settings::load() {
-                Ok(settings_obj) => settings_obj,
-                Err(err) => {
-                    eprintln!("Failed to read configuration file: {}", err);
-                    process::exit(1);
-                }
-            };
-            killjoy::run(&settings_obj);
+        ("settings", Some(sub_args)) => handle_settings_subcommand(&sub_args),
+        _ => handle_no_subcommand(),
+    }
+}
+
+// Handle the 'settings' subcommand.
+fn handle_settings_subcommand(args: &ArgMatches) {
+    match args.subcommand() {
+        ("load-path", Some(_)) => handle_settings_load_path_subcommand(),
+        ("validate", Some(_)) => handle_settings_validate_subcommand(),
+        _ => eprintln!("An unexpected code path executed. Please contact the developer."),
+    }
+}
+
+// Handle the 'settings load-path' subcommand.
+fn handle_settings_load_path_subcommand() {
+    let load_path = match settings::get_load_path() {
+        Ok(load_path) => load_path,
+        Err(err) => {
+            eprintln!("{}", err);
+            process::exit(1);
+        }
+    };
+    println!("{}", load_path);
+}
+
+// Handle the 'settings validate' subcommand.
+fn handle_settings_validate_subcommand() {
+    println!("Handling validate subcommand");
+}
+
+// Handle no subcommand at all.
+fn handle_no_subcommand() {
+    killjoy::run(&get_settings_or_exit());
+}
+
+// Get and return a settings object, or print a message to stderr and exit with a non-zero code.
+fn get_settings_or_exit() -> Settings {
+    match settings::load() {
+        Ok(settings_obj) => settings_obj,
+        Err(err) => {
+            eprintln!("{}", err);
+            process::exit(1);
         }
     }
 }
