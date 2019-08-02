kennep/killjoy

commit cc467c504e65794192e7733e603d76110482c25a

Cast argument to type earlier in runtime
When a user executes `killjoy settings validate path/to/settings.json`,
the last argument is currently represented as a string. This works, but
it's overly lax. Change the code base, so that the path is wrapped in a
`std::path::Path` object early at runtime. This stricter typing makes
the code more semantically rich and helps to catch certain types of
errors earlier during runtime.

src/main.rs
@@ -55,6 +55,7 @@ mod generated;
 mod settings;
 mod unit;
 
+use std::path::Path;
 use std::process;
 use std::thread;
 
@@ -90,12 +91,16 @@ fn handle_settings_load_path_subcommand() {
             process::exit(1);
         }
     };
-    println!("{}", load_path);
+    println!("{:?}", load_path);
 }
 
 // Handle the 'settings validate' subcommand.
 fn handle_settings_validate_subcommand(args: &ArgMatches) {
-    get_settings_or_exit(args.value_of("path"));
+    let path = match args.value_of("path") {
+        Some(path_str) => Some(Path::new(path_str)),
+        None => None,
+    };
+    get_settings_or_exit(path);
 }
 
 // Handle no subcommand at all.
@@ -118,7 +123,7 @@ fn handle_no_subcommand() {
 }
 
 // Get and return a settings object, or print a message to stderr and exit with a non-zero code.
-fn get_settings_or_exit(path: Option<&str>) -> Settings {
+fn get_settings_or_exit(path: Option<&Path>) -> Settings {
     match settings::load(path) {
         Ok(settings_obj) => settings_obj,
         Err(err) => {

src/error.rs
@@ -42,17 +42,3 @@ impl Display for ParseConfigFileError {
 }
 
 impl Error for ParseConfigFileError {}
-
-// An error indicating that a file path could not be parsed.
-//
-// OS file paths are most commonly parsed as unicode.
-#[derive(Debug)]
-pub struct ParsePathError;
-
-impl Display for ParsePathError {
-    fn fmt(&self, f: &mut Formatter) -> FmtResult {
-        write!(f, "Failed to convert file path to a unicode string.")
-    }
-}
-
-impl Error for ParsePathError {}

src/settings.rs
@@ -5,12 +5,13 @@ use std::convert::TryFrom;
 use std::error::Error;
 use std::fs::File;
 use std::io::{BufReader, Read};
+use std::path::{Path, PathBuf};
 
 use dbus::{BusName, BusType};
 use serde::Deserialize;
 use xdg::BaseDirectories;
 
-use crate::error::{FindConfigFileError, ParseConfigFileError, ParsePathError};
+use crate::error::{FindConfigFileError, ParseConfigFileError};
 use crate::unit::ActiveState;
 
 // The expressions that a user may use to match unit names.
@@ -283,7 +284,7 @@ pub fn get_bus_types(rules: &[Rule]) -> Vec<BusType> {
 // Search several paths for a settings file, in order of preference.
 //
 // If a file is found, return its path. Otherwise, return an error describing why.
-pub fn get_load_path() -> Result<String, Box<dyn Error>> {
+pub fn get_load_path() -> Result<PathBuf, Box<dyn Error>> {
     let dirs = match BaseDirectories::with_prefix("killjoy") {
         Ok(dirs) => dirs,
         Err(err) => return Err(Box::new(err)),
@@ -292,11 +293,7 @@ pub fn get_load_path() -> Result<String, Box<dyn Error>> {
         Some(path_buf) => path_buf,
         None => return Err(Box::new(FindConfigFileError)),
     };
-    let path = match path_buf.to_str() {
-        Some(path) => path.to_string(),
-        None => return Err(Box::new(ParsePathError)),
-    };
-    Ok(path)
+    Ok(path_buf)
 }
 
 // Read the configuration file into a Settings object.
@@ -306,12 +303,12 @@ pub fn get_load_path() -> Result<String, Box<dyn Error>> {
 // *   The file couldn't be opened. Maybe a settings file couldn't be found; or maybe a settings
 //     file was found but could not be opened.
 // *   The file contained invalid contents. See [Settings::new](struct.Settings.html#method.new).
-pub fn load(path: Option<&str>) -> Result<Settings, Box<dyn Error>> {
-    let load_path = match path {
-        Some(path) => path.to_owned(),
-        None => get_load_path()?,
+pub fn load(path: Option<&Path>) -> Result<Settings, Box<dyn Error>> {
+    let handle_opt = match path {
+        Some(path) => File::open(path),
+        None => File::open(get_load_path()?.as_path()),
     };
-    let handle = match File::open(load_path) {
+    let handle = match handle_opt {
         Ok(handle) => handle,
         Err(err) => return Err(Box::new(err)),
     };

commit 0dac5f8e116acf29403577d169eda252c035cf8d

Correctly display load-path
Make the `killjoy settings load-path` correctly display the settings
file load path. This error was introduced in
cc467c504e65794192e7733e603d76110482c25a

src/main.rs
@@ -91,7 +91,7 @@ fn handle_settings_load_path_subcommand() {
             process::exit(1);
         }
     };
-    println!("{:?}", load_path);
+    println!("{}", load_path.as_path().display());
 }
 
 // Handle the 'settings validate' subcommand.