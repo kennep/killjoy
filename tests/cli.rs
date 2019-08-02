3ac03ee6831a989c470765af04e467b79c953014
Jeremy Audet <jerebear@protonmail.com>
2019-08-02 13:22:50 -0400

    Test `killjoy settings load-path`
    Test with integration tests.

--- tests/cli.rs
+++ tests/cli.rs
@@ -3,16 +3,56 @@
 use std::fs;
 use std::fs::File;
 use std::io::Write;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 use std::process::{Command, Output};
 
 use assert_cmd::prelude::{CommandCargoExt, OutputAssertExt};
 use tempfile::{NamedTempFile, TempDir};
 
+// Call `killjoy settings load-path` and expect failure.
+#[test]
+fn test_settings_load_path_failure() {
+    let (config_dir, settings_dir, _config_file) = create_config_skeleton();
+    let old_config_file_path = settings_dir.join("settings.json");
+    let new_config_file_path = settings_dir.join("foo.json");
+    std::fs::rename(&old_config_file_path, &new_config_file_path)
+        .expect("Failed to rename settings file.");
+    let config_dir_str = config_dir
+        .path()
+        .to_str()
+        .expect("Failed to convert file path to unicode string.");
+    let out: Output = Command::cargo_bin("killjoy")
+        .expect("Failed to find crate-local executable.")
+        .env("XDG_CONFIG_HOME", config_dir_str)
+        .env("XDG_CONFIG_DIRS", config_dir_str)
+        .args(&["settings", "load-path"])
+        .output()
+        .expect("Failed to run killjoy.");
+    out.assert().failure();
+}
+
+// Call `killjoy settings load-path` and expect success.
+#[test]
+fn test_settings_load_path_success() {
+    let (config_dir, _, _) = create_config_skeleton();
+    let config_dir_str = config_dir
+        .path()
+        .to_str()
+        .expect("Failed to convert file path to unicode string.");
+    let out: Output = Command::cargo_bin("killjoy")
+        .expect("Failed to find crate-local executable.")
+        .env("XDG_CONFIG_HOME", config_dir_str)
+        .env("XDG_CONFIG_DIRS", config_dir_str)
+        .args(&["settings", "load-path"])
+        .output()
+        .expect("Failed to run killjoy.");
+    out.assert().success();
+}
+
 // Call `killjoy settings validate` and expect failure.
 #[test]
 fn test_settings_validate_failure() {
-    let (config_dir, mut settings_file) = create_config_skeleton();
+    let (config_dir, _, mut settings_file) = create_config_skeleton();
     write_invalid_settings(&mut settings_file);
     let config_dir_str = config_dir
         .path()
@@ -31,7 +71,7 @@ fn test_settings_validate_failure() {
 // Call `killjoy settings validate` and expect success.
 #[test]
 fn test_settings_validate_success() {
-    let (config_dir, mut settings_file) = create_config_skeleton();
+    let (config_dir, _, mut settings_file) = create_config_skeleton();
     write_valid_settings(&mut settings_file);
     let config_dir_str = config_dir
         .path()
@@ -91,8 +131,9 @@ fn test_settings_validate_path_success() {
 
 // Create a temporary directory containing "killjoy/settings.json".
 //
-// The settings file is empty. The returned tuple is of the form `(temp_dir, settings_file)`.
-fn create_config_skeleton() -> (TempDir, File) {
+// The settings file is empty. The returned tuple is of the form `(temp_dir, settings_dir,
+// settings_file)`.
+fn create_config_skeleton() -> (TempDir, PathBuf, File) {
     let xdg_config_home = TempDir::new().expect("Failed to create xdg_config_home.");
 
     let settings_dir = xdg_config_home.path().join(Path::new("killjoy"));
@@ -101,7 +142,7 @@ fn create_config_skeleton() -> (TempDir, File) {
     let settings_file = File::create(settings_dir.join(Path::new("settings.json")))
         .expect("Failed to create settings_file.");
 
-    (xdg_config_home, settings_file)
+    (xdg_config_home, PathBuf::from(settings_dir), settings_file)
 }
 
 fn write_invalid_settings<T: Write>(handle: &mut T) {
