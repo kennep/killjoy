3a761c82e6d9f886df5651eadf786c102383bab8
Jeremy Audet <jerebear@protonmail.com>
2019-08-16 18:58:02 -0400

    Add integration tests for settings subcommand
    Assert that killjoy exits gracefully when the settings file is absent or
    unreadable. This helps ensure that the
    `SettingsFileError::{FileNotFound, FileNotReadable}` errors are
    correctly propagated.

--- tests/cli.rs
+++ tests/cli.rs
@@ -48,10 +48,61 @@ fn test_settings_load_path_success() {
 
 // Call `killjoy settings validate` and expect failure due to the settings file being invalid.
 #[test]
-fn test_settings_validate_failure() {
+fn test_settings_validate_failure_v1() {
     let (config_dir, _, mut settings_file) = create_skeleton_config();
     write_invalid_settings(&mut settings_file);
     let config_dir_str = path_to_str(&config_dir.path());
+    Command::cargo_bin("killjoy")
+        .expect("Failed to find crate-local executable.")
+        .env("XDG_CONFIG_HOME", config_dir_str)
+        .env("XDG_CONFIG_DIRS", config_dir_str)
+        .args(&["settings", "validate"])
+        .output()
+        .expect("Failed to run killjoy.")
+        .assert()
+        .code(1);
+}
+
+// Call `killjoy settings validate` and expect failure due to the settings file being absent.
+#[test]
+fn test_settings_validate_failure_v2() {
+    let (config_dir, settings_dir, _) = create_skeleton_config();
+    let old_settings_file_path = settings_dir.join("settings.json");
+    let new_settings_file_path = settings_dir.join("foo.json");
+    std::fs::rename(&old_settings_file_path, &new_settings_file_path)
+        .expect("Failed to rename settings file.");
+    let config_dir_str = path_to_str(&config_dir.path());
+    Command::cargo_bin("killjoy")
+        .expect("Failed to find crate-local executable.")
+        .env("XDG_CONFIG_HOME", config_dir_str)
+        .env("XDG_CONFIG_DIRS", config_dir_str)
+        .args(&["settings", "validate"])
+        .output()
+        .expect("Failed to run killjoy.")
+        .assert()
+        .code(1);
+}
+
+// Call `killjoy settings validate` and expect failure due to the settings file being unreadable.
+#[test]
+fn test_settings_validate_failure_v3() {
+    let (config_dir, settings_dir, _) = create_skeleton_config();
+    let config_dir_str = path_to_str(&config_dir.path());
+    // PermissionsExt::set_mode has no effect. Maybe it's a bug in Rust?
+    Command::new("chmod")
+        .env("XDG_CONFIG_HOME", config_dir_str)
+        .env("XDG_CONFIG_DIRS", config_dir_str)
+        .args(&[
+            "000",
+            Path::new(&settings_dir)
+                .join("settings.json")
+                .to_str()
+                .expect("Failed to cast file path to UTF-8."),
+        ])
+        .output()
+        .expect("Failed to run chmod.")
+        .assert()
+        .code(0);
     Command::cargo_bin("killjoy")
         .expect("Failed to find crate-local executable.")
         .env("XDG_CONFIG_HOME", config_dir_str)
