0a5a030b5c0b04b7c95ac0b50d6de3a033c3e8ba
Jeremy Audet <jerebear@protonmail.com>
2019-08-19 18:37:50 -0400

    Update Travis CI checks

--- scripts/travis-checks.sh
+++ scripts/travis-checks.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 find scripts -type f -name '*.sh' -exec shellcheck '{}' ';'
-cargo check
-cargo test
+cargo check --locked --profile test
+cargo test --locked
 cargo clippy -- --deny warnings  # treat warnings as errors
 cargo fmt && git diff --exit-code
