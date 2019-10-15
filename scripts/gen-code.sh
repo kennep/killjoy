c26fcb7d511ffd4163b741f08556639c538f238f
Jeremy Audet <jerebear@protonmail.com>
2019-10-15 13:21:54 -0400

    Update script for regenerating code
    Let the script automatically fix several deficiencies in the generated
    code after generating the code.

--- scripts/gen-code.sh
+++ scripts/gen-code.sh
@@ -8,3 +8,6 @@ set -euo pipefail
     --destination org.freedesktop.systemd1 \
     --path /org/freedesktop/systemd1 \
     > src/generated/org_freedesktop_systemd1.rs
+sed -i 's/<arg::RefArg/<dyn arg::RefArg/g' \
+    src/generated/org_freedesktop_systemd1.rs
+cargo fmt
